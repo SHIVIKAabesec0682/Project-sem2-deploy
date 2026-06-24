5ef37afb1c09abfe01c594bc364338f4ce5a63c4
Shivika Chauhan <shivika.chauhan3103@gmail.com>
2026-06-24 10:56:32 +0530

    data

---
+++ src/electronicsData.js
@@ -0,0 +1,36 @@
+export const electronicsData = [
+  {
+    id: "crm-lp-01",
+    name: "MacBook Pro 14-inch M3",
+    brand: "Apple",
+    price: 169900,
+    slashedPrice: 199900,
+    category: "Laptops",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQck7SjOxHcgAcztrtXlvx8In8YEMwhK7ulcfLkzYhAqQ&s=10",
+    rating: 4.8,
+    reviewsCount: 312,
+    specs: {
+      "Processor": "Apple M3 Chip",
+      "RAM": "16GB Unified Memory",
+      "Storage": "512GB SSD",
+      "OS": "macOS Sonoma"
+    }
+  },
+  {
+    id: "crm-mb-02",
+    name: "Samsung Galaxy S26 Ultra",
+    brand: "Samsung",
+    price: 124999,
+    slashedPrice: 134999,
+    category: "Mobiles",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRHHxBA9g-AQHDNPe6KajCQSQCSqM9m7XPK-UPRO8SjQ&s=10",
+    rating: 4.7,
+    reviewsCount: 184,
+    specs: {
+      "Display": "6.8-inch Dynamic AMOLED",
+      "Camera": "200MP Main Lens",
+      "Battery": "5000 mAh",
+      "Network": "5G Enabled"
+    }
+  }
+];
